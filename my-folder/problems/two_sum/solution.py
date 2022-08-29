cde6a3b376e6af353bbe12536403d4c54ecb1717
m-weary <111722893+m-weary@users.noreply.github.com>
2022-08-29 23:43:22 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,10 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums) - 1):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    solution = [i,j]
-        return solution
+        #Can use a hashmap. Map Value to Index
+        hm = {}
+        
+        for index, value in enumerate(nums):
+            diff = target - value
+            if diff in hm:
+                return [hm[diff],index]
+            else:
+                hm[value] = index
             
-            
-            
+        
+        
+                
+                
