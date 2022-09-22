bf6c03903b735227d37ec5a4d9b9703864c626ce
m-weary <111722893+m-weary@users.noreply.github.com>
2022-09-22 12:57:36 -0700

    Time: 81 ms (5.71%), Space: 13.9 MB (56.06%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -1,12 +1,12 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
         """A key element of this question is the array can be any combination"""
-        arr = []
-        sum_arr = 0
-        for i in range(1,n):
-            val = i
+        arr = [] #create an empty array
+        sum_arr = 0 #track the sum of the array
+        for i in range(1,n): #start the loop at 1 to eliminate edge case of n = 1 or 2
+            val = i 
             arr.append(val)
             sum_arr += val
-        arr.append(-(sum_arr))
+        arr.append(-(sum_arr)) #Add the inverse of the sum of the array as the last value 
         return arr
             
